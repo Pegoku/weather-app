ad5f3639eebc05eafec151d6eb971e386d233b99
Pegoku <pegoku08@gmail.com>
2024-07-06 12:04:31 +0200

    finished

--- README.md
+++ README.md
@@ -1,1 +1,25 @@
 # weather-app
+
+This Weather App provides real-time weather updates for today and the upcoming week. It utilizes the OpenWeatherMap API to fetch weather data and displays it in a user-friendly web interface.
+![weather-app](https://raw.githubusercontent.com/Pegoku/weather-app/main/img/image.png)
+
+## Features
+
+- **Today's Weather**: Get the current weather conditions for your area including temperature and weather description.
+- **Weekly Weather Forecast**: View the weather forecast for the next seven days, including temperature highs and lows and weather conditions.
+
+
+## Installation
+
+1. Clone this repository: `git clone https://github.com/Pegoku/weather-app.git`
+2. Navigate to the project directory: `cd weather-app`
+3. Install the required packages: `pip install -r requirements.txt`
+4. Run the application: `python app.py`
+
+## Usage
+
+Open your web browser and navigate to `http://localhost:5000` to start using the application.
+
+## Contributing
+
+Pull requests are welcome. For major changes, please open an issue first to discuss what you would like to change.
--- app.py
+++ app.py
@@ -17,6 +17,9 @@ def create_db_and_table():
     c.execute('''CREATE TABLE IF NOT EXISTS weather_today
                  (date TEXT, city TEXT, temperature REAL, description TEXT)''')
     conn.commit()
+    c.execute('''CREATE TABLE IF NOT EXISTS weather_week
+                 (date TEXT, city TEXT, temperature REAL, description TEXT)''')
+    conn.commit()
     conn.close()
 
 def get_weather_today_api():
@@ -98,6 +101,85 @@ def get_weather_today():
     
     return get_weather_today_api()
 
+
+def get_weather_week_db():
+    conn = sqlite3.connect('weather.db')
+    c = conn.cursor()
+
+    c.execute("SELECT date, city, temperature, description FROM weather_week")
+    
+    weather_data = c.fetchall()
+
+    conn.close()
+
+    # Convert the fetched data into a structured format
+    weather_list = []
+    for row in weather_data:
+        weather_dict = {
+            "date": row[0],
+            "city": row[1],
+            "temperature": row[2],
+            "description": row[3]
+        }
+        weather_list.append(weather_dict)
+
+    return weather_list
+
+def get_weather_week_api():
+    complete_url = f"{BASE_URL}forecast?q={CITY_NAME}&appid={API_KEY}&units=metric"
+    response = requests.get(complete_url)
+    weather_data = response.json()
+
+    print("Pulling data from API")
+
+    if weather_data['cod'] == "200":
+        conn = sqlite3.connect('weather.db')
+        c = conn.cursor()
+
+        # Clear existing data
+        c.execute("DELETE FROM weather_week")
+        conn.commit()
+
+        # Filter forecasts for 09:00 and 18:00 only
+        filtered_forecasts = [forecast for forecast in weather_data['list'] if forecast['dt_txt'].endswith("09:00:00") or forecast['dt_txt'].endswith("18:00:00")]
+
+        for forecast in filtered_forecasts:
+            date = forecast['dt_txt']
+            temperature = forecast['main']['temp']
+            description = forecast['weather'][0]['description']
+            city = CITY_NAME
+            c.execute("INSERT INTO weather_week (date, city, temperature, description) VALUES (?, ?, ?, ?)",
+                      (date, city, temperature, description))
+
+        conn.commit()
+        conn.close()
+        print("Data stored in DB")
+
+        return filtered_forecasts  # Return the filtered list of forecasts
+    else:
+        return None
+
+def get_weather_week():
+    conn = sqlite3.connect('weather.db')
+    c = conn.cursor()
+    c.execute("SELECT date FROM weather_week ORDER BY date ASC LIMIT 1") 
+    first_entry = c.fetchone()
+    conn.close()
+
+    if first_entry:
+        first_date = datetime.strptime(first_entry[0], "%Y-%m-%d %H:%M:%S")
+        current_date = datetime.now()
+        time_difference = current_date - first_date
+        if time_difference.total_seconds() < 604800:  # 7 days
+            print("Data is up to date")
+            return get_weather_week_db()
+        else:
+            print("Data is not up to date")
+            return get_weather_week_api()
+    
+    return get_weather_week_api()
+
+
 @app.route('/')
 def home():
     return render_template('home.html')
@@ -114,6 +196,10 @@ def today_weather():
 @app.route('/week')
 def week_weather():
     weather_data = get_weather_week()
+    if weather_data is None:
+        # Handle the case where weather_data is None by using an empty list
+        weather_data = []
+    print(weather_data)
     return render_template('week.html', weather_data=weather_data)
 
 # if weather.db doesn't exist
@@ -128,4 +214,4 @@ create_db_and_table()
 
 
 if __name__ == '__main__':
-    app.run(debug=True)
+    app.run(debug=True, host='0.0.0.0', port=5000)
